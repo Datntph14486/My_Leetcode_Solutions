0d41337afcc2c8b24cd642d64e7bec95efed48e8
Dat Nguyen <dat.nguyen@ohmidasvn.com>
2024-11-12 08:58:19 +0700

    Answer question 2070

---
+++ problems/2070. Most Beautiful Item for Each Query/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} items
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+var maximumBeauty = function (items, queries) {
+    items.sort((a, b) => b[1] - a[1]);
+
+    const result = [];
+
+    for (let i = 0; i < queries.length; i++) {
+        let beauty = 0;
+
+        for (let j = 0; j < items.length; j++) {
+            const currentBeauty = items[j];
+
+            if (currentBeauty[0] <= queries[i] && currentBeauty[1] > beauty) {
+                beauty = currentBeauty[1];
+                break;
+            }
+        }
+
+        result.push(beauty);
+    }
+
+    return result;
+};
