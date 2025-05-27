7b07568a3bca663011feb1496d732982e870e033
Datntph14486 <101643961+Datntph14486@users.noreply.github.com>
2025-05-27 14:08:47 +0700

    Update soulution.js

--- problems/2. Add Two Numbers/soulution.js
+++ problems/2. Add Two Numbers/soulution.js
@@ -1,34 +1,49 @@
-function ListNode(val, next) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-}
-
-let mod = 0;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    if (!l1 && !l2 && !mod) {
-        return undefined;
+    let str1 = getNode(l1, '');
+    let str2 = getNode(l2, '');
+
+    const sum = BigInt(str1) + BigInt(str2);
+
+    const reverseSum = String(sum).split('').reverse().join('');
+
+    let result = null;
+    let currentNode = null;
+
+    for (let i = 0; i < reverseSum.length; i++) {
+        const newNode = new ListNode(Number(reverseSum[i]));
+
+        if (result == null) {
+            result = newNode
+            currentNode = result;
+            continue;
+        }
+
+        currentNode.next = newNode;
+        currentNode = currentNode.next;
     }
 
-    let sum = 0;
+    return result;
 
-    if (l1 && !l2) {
-        sum = l1.val + mod;
-    } else if (l2 && !l1) {
-        sum = l2.val + mod;
-    } else if (l2 && l1) {
-        sum = l1.val + l2.val + mod;
-    } else {
-        sum = mod;
-    }
-
-    result = sum % 10;
-
-    mod = sum >= 10 ? 1 : 0;
-
-    return new ListNode(result, addTwoNumbers(l1?.next, l2?.next));
 };
+
+function getNode(node, str) {
+    if (!node) {
+        return str.split('').reverse().join('');
+    }
+    str += node.val;
+
+    return getNode(node.next, str);
+
+}
